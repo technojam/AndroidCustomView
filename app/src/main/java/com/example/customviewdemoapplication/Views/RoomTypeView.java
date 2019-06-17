82e249586a76a6e3879111abb486e9cb42e1d760
DANISH <developer.danish.jamal@gmail.com>
2019-06-17 15:59:41 +0530

    Touched implemented

--- app/src/main/java/com/example/customviewdemoapplication/Views/RoomTypeView.java
+++ app/src/main/java/com/example/customviewdemoapplication/Views/RoomTypeView.java
@@ -10,15 +10,20 @@ import android.graphics.Picture;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.Pair;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.example.customviewdemoapplication.R;
+
+import java.util.HashMap;
 
 public class RoomTypeView extends View {
 
     static float ADD_RIGHT_MARGIN = 300f;
     static float ADD_SUBTRACT_COUNTER_GAP = 80f;
 
+    static int TOUCH_AREA = 30;
 
 
     String mText;
@@ -33,6 +38,8 @@ public class RoomTypeView extends View {
     Paint mPaintCircle;
     Paint mPaintText;
     Paint mPaintCounterText;
+
+    HashMap<String, Float> mPosition = new HashMap<>();
 
 
 
@@ -56,8 +63,6 @@ public class RoomTypeView extends View {
 
         typedArray.recycle();
 
-
-
         mPaintCircle = new Paint();
         mPaintCircle.setColor(Color.BLACK);
 
@@ -68,8 +73,6 @@ public class RoomTypeView extends View {
         mPaintCounterText = new Paint();
         mPaintCounterText.setColor(mCounterTextColor);
         mPaintCounterText.setTextSize(mTextSize);
-
-
 
 
     }
@@ -91,6 +94,42 @@ public class RoomTypeView extends View {
 
         canvas.drawText(">", getWidth()-ADD_RIGHT_MARGIN+2*ADD_SUBTRACT_COUNTER_GAP- mNumberPickerRightGap, getHeight()/2 + mTextSize/2 - mCircleRadius, mPaintCounterText);
 
+        mPosition.put("DECREMENT_X", getWidth()-ADD_RIGHT_MARGIN- mNumberPickerRightGap);
+        mPosition.put("INCREMENT_X", getWidth()-ADD_RIGHT_MARGIN+2*ADD_SUBTRACT_COUNTER_GAP- mNumberPickerRightGap);
+
+
 
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean value = super.onTouchEvent(event);
+
+        float x = event.getX();
+
+        switch (event.getAction()){
+
+            case MotionEvent.ACTION_DOWN:
+
+                if(x >= mPosition.get("DECREMENT_X")-TOUCH_AREA && x <= mPosition.get("DECREMENT_X")+TOUCH_AREA){
+                    if(mCounter > 0){
+                        mCounter -= 1;
+                        postInvalidate();
+                    }
+                }
+
+                if(x >= mPosition.get("INCREMENT_X")-TOUCH_AREA && x <= mPosition.get("INCREMENT_X")+TOUCH_AREA){
+                    if(mCounter < 9){
+                        mCounter += 1;
+                        postInvalidate();
+                    }
+                }
+                break;
+
+        }
+
+        return value;
+    }
+
+    
 }
