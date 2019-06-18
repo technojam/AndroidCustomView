4908beb05cc464801b9a72de275c32dd4d2fbfa0
DANISH <developer.danish.jamal@gmail.com>
2019-06-18 19:23:54 +0530

    ReviewBarView class file added

---
+++ app/src/main/java/com/example/customviewdemoapplication/Views/ReviewBarView.java
@@ -0,0 +1,19 @@
+package com.example.customviewdemoapplication.Views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ReviewBarView extends View {
+
+    public ReviewBarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+
+    private void init(AttributeSet set){
+        
+    }
+}
