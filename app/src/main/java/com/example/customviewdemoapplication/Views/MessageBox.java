a8a8775ff95177667887a575a440f8f6e68e91f3
DANISH <developer.danish.jamal@gmail.com>
2019-08-24 16:27:19 +0530

    Meassage bubble box implemented

--- app/src/main/java/com/example/customviewdemoapplication/Views/MessageBox.java
+++ app/src/main/java/com/example/customviewdemoapplication/Views/MessageBox.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -14,6 +15,8 @@ public class MessageBox extends View {
 
     Paint paint;
     Path path;
+
+    Paint textPaint;
 
     int width;
     int height;
@@ -26,8 +29,13 @@ public class MessageBox extends View {
     private void init(AttributeSet attrs){
         paint = new Paint();
         paint.setColor(Color.RED);
-        paint.setStrokeWidth(10);
+        paint.setStrokeWidth(5);
         paint.setStyle(Paint.Style.STROKE);
+
+        textPaint = new Paint();
+        textPaint.setColor(Color.GREEN);
+        textPaint.setTextSize(40);
+        textPaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
 
 
     }
@@ -42,7 +50,7 @@ public class MessageBox extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.translate(50f, 50f);
+        canvas.translate(20f, 50f);
 
         path = new Path();
 
@@ -53,11 +61,36 @@ public class MessageBox extends View {
         path.lineTo(0, 0);
          */
 
-        RectF oval = new RectF();
+        /*
+        path.addArc(0f, 0f, 0+100, 0+100, 180, 90);
+        path.lineTo(width-100, 0);
+        path.addArc(width-100-50, 0f, width-100+50, 100, 270, 90);
+        path.lineTo(width-100+50, 200);
+        path.addArc(width-100-50, 200-50, width-100+50, 50+200, 0, 90);
+        path.lineTo(50, 200+50);
+        path.addArc(0, 200-50, 100, 200+50, 90, 90);
+        path.lineTo(0, 50);
 
-        path.addArc(0f, 0f, (0+500), (0+500), 180+40, 45);
+         */
 
+        int w = width-200;
+        int h = 100;
+
+        path.addArc(0f, 0f, 0+100, 0+100, 180, 90);
+        path.lineTo(w, 0);
+
+        path.addArc(w-50, 0f, w+50, 100, 270, 90);
+        path.lineTo(w+50, h);
+
+        path.addArc(w+50, h-50, w+50+50, 50+h, 90, 90);
+        path.moveTo(w+70, h+50);
+        path.lineTo(50, h+50);
+
+
+        path.addArc(0, h-50, 100, h+50, 90, 90);
+        path.lineTo(0, 50);
 
         canvas.drawPath(path, paint);
+        canvas.drawText("Hello", 50, 50, textPaint);
     }
 }
