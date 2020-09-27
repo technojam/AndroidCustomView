3c98acf4a378eed8e2d44e8e0c543a1ef30e3347
abhi114 <nabhi531@gmail.com>
2020-09-28 00:33:29 +0530

    closes: #4

--- app/src/main/java/com/example/customviewdemoapplication/MainActivity.java
+++ app/src/main/java/com/example/customviewdemoapplication/MainActivity.java
@@ -15,30 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
     private MyView myView;
     private ColorView mColorView;
-    //private static final String[] COUNTRIES = new String[]{"asdasdas","aasdasd","weqeqwreqewr","asfasgfqasgwqr"};
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*final RoomTypeView roomTypeView = (RoomTypeView)findViewById(R.id.roomTypeView);
-        Button get = (Button)findViewById(R.id.getButton);
-
-        get.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, ""+roomTypeView.getmCounter(), Toast.LENGTH_SHORT).show();
-            }
-        });*/
-
-       /* AutoCompleteTextView textView =  findViewById(R.id.autoCompleteCountry); // get a reference to the autocomplete in the layout
-
-        String[] countries = getResources().getStringArray(R.array.countries_array);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,COUNTRIES);
-        Toast.makeText(MainActivity.this,"sadasd",Toast.LENGTH_LONG).show();
-        textView.setAdapter(adapter);*/
+        
         mColorView = (ColorView) findViewById(R.id.colorView);
 
         Button btn =(Button) findViewById(R.id.btn_swap_color);
