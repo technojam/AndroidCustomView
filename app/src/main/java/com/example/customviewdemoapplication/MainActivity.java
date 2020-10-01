65ee7a20d8e0237c128f71d7e3591b28fc30649b
Danish Jamal <danishjamal.104@gmail.com>
2020-10-01 15:15:15 +0530

    Added text_size as a attribute in Progress, viz responsible for changing its size.

--- app/src/main/java/com/example/customviewdemoapplication/MainActivity.java
+++ app/src/main/java/com/example/customviewdemoapplication/MainActivity.java
@@ -1,28 +1,23 @@
 package com.example.customviewdemoapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.customviewdemoapplication.Views.ColorView;
-import com.example.customviewdemoapplication.Views.MyView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MyView myView;
     private ColorView mColorView;
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
         mColorView = (ColorView) findViewById(R.id.colorView);
 
         Button btn =(Button) findViewById(R.id.btn_swap_color);
@@ -32,9 +27,6 @@ public class MainActivity extends AppCompatActivity {
                 mColorView.SwapColor();
             }
         });
-
-
-
 
     }
 }
